7a87c6e32885a087a4c2882fefe74d8615d13344
Chip Eyler <freqnlabs@gmail.com>
2015-10-28 07:30:03 -0400

    Sketch first basic todo model

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,25 @@
 'use strict';
 
-var AppView = Backbone.View.extend({
-	el: $('#container'),
-	template: _.template('<h3>Wowzers, <%= who %> <h3>'),
-	initialize: function(){
-		this.render();
-	},
-	render: function(){
-		this.$el.html(this.template({who: "Look Mah.. I can backbone!!!!"}));
-	}
+var app = {}; // create namespace for our app
+
+app.Todo = Backbone.Model.extend({
+ defaults: {
+	 title: '',
+	 completed: false
+ }
 });
-var appView = new AppView();
+
+
+
+
+// var AppView = Backbone.View.extend({
+// 	el: $('#container'),
+// 	template: _.template('<h3>Wowzers, <%= who %> <h3>'),
+// 	initialize: function(){
+// 		this.render();
+// 	},
+// 	render: function(){
+// 		this.$el.html(this.template({who: "Look Mah.. I can backbone!!!!"}));
+// 	}
+// });
+// var appView = new AppView();
